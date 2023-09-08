7a767c6193558e10e8def7ce70f270394a6557fb
Akash Kumar Singh <itsakash18.06@gmail.com>
2023-09-08 22:26:27 +0530

    React router dom added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
+import { Routes, Route } from 'react-router-dom'
+import Home from './Components/Home'
+import About from './Components/About'
+import Contact from './Components/Contact'
+import Project from './Components/Project'
+import Skills from './Components/Skills'
 
 const App = () => {
   return (
-    <div className="text-red-500">
-      hello
-    </div>
+    <Routes>
+      <Route path='/Akash1806' element={<Home />} />
+      <Route path='/Akash1806/about' element={<About />} />
+      <Route path='/Akash1806/Contact' element={<Contact />} />
+      <Route path='/Akash1806/Project' element={<Project />} />
+      <Route path='/Akash1806/Skills' element={<Skills />} />
+    </Routes>
   )
 }
 
--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -2,7 +2,7 @@
 const About = () => {
     return (
         <div>
-            about page
+            about
         </div>
     )
 }
