e1099a38346ccc9a78eb0a84576c66d70602a47f
Akash Kumar Singh <itsakash18.06@gmail.com>
2023-09-08 22:43:54 +0530

    No page component added

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,17 @@ import About from './Components/About'
 import Contact from './Components/Contact'
 import Project from './Components/Project'
 import Skills from './Components/Skills'
+import NoPage from './Components/NoPage'
 
 const App = () => {
   return (
     <Routes>
       <Route path='/Akash1806' element={<Home />} />
       <Route path='/Akash1806/about' element={<About />} />
-      <Route path='/Akash1806/Contact' element={<Contact />} />
-      <Route path='/Akash1806/Project' element={<Project />} />
-      <Route path='/Akash1806/Skills' element={<Skills />} />
+      <Route path='/Akash1806/contact' element={<Contact />} />
+      <Route path='/Akash1806/project' element={<Project />} />
+      <Route path='/Akash1806/skills' element={<Skills />} />
+      <Route path='*' element={<NoPage />} />
     </Routes>
   )
 }
---
+++ src/Components/NoPage.jsx
@@ -0,0 +1,10 @@
+
+const NoPage = () => {
+    return (
+        <div>
+            404 no page found
+        </div>
+    )
+}
+
+export default NoPage
