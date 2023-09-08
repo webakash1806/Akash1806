9beb0e205b1ea2d27b95c0e67eca16bafa86ebfe
Akash Kumar Singh <itsakash18.06@gmail.com>
2023-09-08 22:37:54 +0530

    Header basic created

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,14 @@
+import { Link } from "react-router-dom"
 
 const Header = () => {
     return (
-        <div>
-            header
-        </div>
+        <>
+            <Link to="/Akash1806">Home</Link>
+            <Link to="/Akash1806/about">About</Link>
+            <Link to="/Akash1806/contact">Home</Link>
+            <Link to="/Akash1806/project">Home</Link>
+            <Link to="/Akash1806/skills">Home</Link>
+        </>
     )
 }
 
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,6 +1,9 @@
+import Header from "./Header"
+
 const Home = () => {
     return (
         <div>
+            <Header />
             <p>Home</p>
         </div>
     )
